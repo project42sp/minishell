82d5855f1dd6cec2a640c8c84f2b41c62f391aca
bKaori <bruna.k.u@gmail.com>
2026-02-14 16:32:21 -0300

    fix: minishell.h correction libft path and t_tree struct

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,14 +6,14 @@
 /*   By: buehara <buehara@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/12 19:16:10 by buehara           #+#    #+#             */
-/*   Updated: 2026/02/12 21:50:45 by thfernan         ###   ########.fr       */
+/*   Updated: 2026/02/12 22:36:58 by buehara          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-# include "libft/libft.h"
+# include "../libft/libft.h"
 # include <fcntl.h>
 # include <signal.h>
 # include <stdlib.h>
@@ -23,9 +23,9 @@
 
 typedef struct s_tree
 {
-	void	*node;
-	t_tree	*left;
-	t_tree	*right;
+	void			*node;
+	struct s_tree	*left;
+	struct s_tree	*right;
 }	t_tree;
 
 #endif
