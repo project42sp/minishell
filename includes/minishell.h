136793c4a12af8a161e8fbc01a1ccb7bf856edee
buehara <buehara@student.42sp.org.br>
2026-03-28 19:59:10 -0300

    chore: add get_next_token prototype

--- includes/minishell.h
+++ includes/minishell.h
@@ -36,6 +36,7 @@ void	tree_print_extense(t_tree *tree);
 
 t_tree	*tree_create(t_token *list, t_check *flags);
 t_token	*token_create(char **tokens, t_tokens_type *signal);
+t_token	*get_next_token(t_token *token);
 
 void	tree_print_extense(t_tree *tree); 
 
