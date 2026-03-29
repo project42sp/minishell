c0f4570168d5414ebec2ff114394d29c91b43c4a
buehara <buehara@student.42sp.org.br>
2026-03-28 21:21:59 -0300

    chore: add function tests to a new branch

---
+++ src/tree/tree_print.c
@@ -0,0 +1,19 @@
+#include "../includes/minishell.h"
+
+void	tree_print(t_tree *tree, int level)
+{
+	if (!tree)
+		return ;
+	tree_print(tree->right, level + 1);
+	printf("%*c%s\n", level * 5, ' ', (char *)tree->node);
+	tree_print(tree->left, level + 1);
+}
+
+void	tree_print_extense(t_tree *tree)
+{
+	if (!tree)
+		return ;
+	tree_print_extense(tree->left);
+	ft_printf("%s ", (char *)tree->node);
+	tree_print_extense(tree->right);
+}
