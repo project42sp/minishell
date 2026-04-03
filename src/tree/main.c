03b2d6086f18c19b47cf9c3b974192db52da3e98
Thais Fernandes Rocha <thfernan@c1r3p2.42sp.org.br>
2026-04-02 21:52:06 -0300

    feat: add lexer and test call in main

--- src/tree/main.c
+++ src/tree/main.c
@@ -7,34 +7,31 @@ int	main(void)
 	t_tree	*tree;
 	t_check	flags;
 
-	while(1)
+	while (1)
 	{
 		input = readline("minishell$ ");
-		// ctrk + D para sair do shell
 		if (!input)
-			break;
+			break ;
 		if (!*input)
 		{
 			free(input);
 			continue ;
 		}
-		// Adiciona ao histórico se a entrada não for vazia
-		//add_history(input);
-		// Hardcode - Lexer vai substituir isso depois
-		t_tokens_type	signal[] = {CMD, FILE_PATH, INPUT, OUTPUT, APPEND, HEREDOC, PIPE, AND, OR, EOFILE};
-		char			*tokens[] = {input, NULL};
-		flags.word = 1;
+		flags.word = 0;
 		flags.input = 0;
 		flags.output = 0;
 		flags.pipe = 0;
 		flags.logical = 0;
-		head = token_create(tokens, signal);
+		head = lexer(input, &flags);
 		if (!head)
 		{
 			ft_printf("Failed to create token list\n");
 			free(input);
 			return (1);
 		}
+		debug_lexer(head);
+		ft_printf("flags: word=%d input=%d output=%d pipe=%d logical=%d\n",
+		flags.word, flags.input, flags.output, flags.pipe, flags.logical);
 		tree = tree_create(head, &flags);
 		ft_printf("You entered: %s\n", input);
 		tree_print(tree, 0);
