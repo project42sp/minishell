d868cb573975b8b4e2f211267f54d3ea5a18f790
buehara <buehara@student.42sp.org.br>
2026-04-05 14:56:43 -0300

    fix: add guard to token free list and add function to free empty tokens

--- src/free/token_list_free.c
+++ src/free/token_list_free.c
@@ -9,14 +9,27 @@ void	token_list_free(t_token *head)
 	while (head)
 	{
 		next = head->next;
-		temp = (char **)head->token;
-		index = 0;
-		while (temp && temp[index] != NULL)
-		{
-			free(temp[index]);
-			index++;
+		if (head->token){
+			temp = (char **)head->token;
+			index = 0;
+			while (temp && temp[index] != NULL)
+			{
+				free(temp[index]);
+				index++;
+			}
 		}
-		//free(head->token);
+		free(head);
+		head = next;
+	}
+}
+
+void	token_no_content_free(t_token *head)
+{
+	t_token	*next;
+
+	while (head)
+	{
+		next = head->next;
 		free(head);
 		head = next;
 	}
