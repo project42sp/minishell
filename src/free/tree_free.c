19f3e97055529765e0db821e902008ab210e9dc3
Thais Fernandes Rocha <thfernan@c1r3p2.42sp.org.br>
2026-04-05 18:41:36 -0300

    fix: free node string and pointer in tree_free

--- src/free/tree_free.c
+++ src/free/tree_free.c
@@ -6,5 +6,10 @@ void	tree_free(t_tree *tree)
 		return ;
 	tree_free(tree->left);
 	tree_free(tree->right);
+	if (tree->node)
+	{
+		free(((char **)tree->node)[0]);// libera a string
+		free(tree->node);// libera o char **
+	}
 	free(tree);
 }
