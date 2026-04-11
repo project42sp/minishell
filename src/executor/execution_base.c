9335a77222eab8d3b507fa50fbd89347fbd9fcc5
bKaori <uekabruna@gmail.com>
2026-04-11 10:03:49 -0300

    fix: path correction

--- src/executor/execution_base.c
+++ src/executor/execution_base.c
@@ -17,13 +17,13 @@ static char	**create_path_table(t_envp *envp)
 	return (table);
 }
 
-static char	*find_path(char **path_envp, t_tree *tree)
+static char	*find_path(char **path_envp, char **cmd)
 {
 	char	*path;
 	int		index;
 	char	*full_path;
 
-	path = (char *)tree->node;
+	path = cmd[0];
 	if (!path)
 		return (NULL);
 	if (access(path, X_OK | F_OK) == 0)
@@ -35,10 +35,7 @@ static char	*find_path(char **path_envp, t_tree *tree)
 		if (!full_path)
 			return (NULL);
 		if (access(full_path, F_OK | X_OK) == 0)
-		{
-			free(path);
 			return (full_path);
-		}
 		free(full_path);
 		full_path = NULL;
 		index++;
@@ -46,13 +43,14 @@ static char	*find_path(char **path_envp, t_tree *tree)
 	return (NULL);
 }
 
-static int	child_process(char **envp, t_tree *tree, char *path)
+static int	child_process(char **envp, char **cmd, char *path)
 {
-	execve(path, (char **)tree->node, envp);
+	execve(path, cmd, envp);
+	ft_printf("__Error__");
 	free(path);
-	tree_free(tree);
-	// Funcao free path_envp (char **)
-	perror("Error: ");
+	// free cmd
+	envp_char_free(envp);
+	perror("Error");
 	exit(127);
 }
 
@@ -61,22 +59,25 @@ static int	base_exec(char **envp, t_tree *tree)
 	char	*path;
 	pid_t	pid;
 	int		err;
+	char	**node;
 
 	//if (builtin)
 		//Run builtin
 		// Return status code
-	path = find_path(envp, tree);
+	path = NULL;
+	node = (char **)tree->node;
+	path = find_path(envp, node);
 	if (!path)
 		return (1);
 	pid = fork();
 	if (pid == 0)
 	{
-		err = child_process(envp, tree, path);
+		err = child_process(envp, node, path);
 		return err;
 	}
 	else
 	{
-		perror("Error: ");
+		perror("Error");
 		//Return error for fork function
 		return (127);
 	}
@@ -84,18 +85,18 @@ static int	base_exec(char **envp, t_tree *tree)
 
 int execution(t_tree *tree, t_envp *envp)
 {
-	char	**envp_table;
+	char	**path_table;
 	int		status_error;
 
-	envp_table = create_path_table(envp);
-	if (!envp_table)
+	path_table = create_path_table(envp);
+	if (!path_table)
 		return (1);
 	if (tree->signal == CMD)
 	{
-		status_error = base_exec(envp_table, tree);
+		status_error = base_exec(path_table, tree);
 		return (status_error); // Temp to make the function compile
 	}
-	envp_char_free(envp_table);
+	envp_char_free(path_table);
 	//TODO: Criar o export e adicionar o status_error dentro da variavel $?
 	return (0);
 }
