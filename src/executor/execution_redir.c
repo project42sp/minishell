4c2673573b912b0136d87613ba60f3beab2c88ff
buehara <buehara@student.42sp.org.br>
2026-04-19 18:42:27 -0300

    chore: code flow corrections and create new function base_redir

--- src/executor/execution_redir.c
+++ src/executor/execution_redir.c
@@ -1,5 +1,18 @@
 #include "../../includes/minishell.h"
 
+int	base_redir(t_tree *tree, int *fd)
+{
+	if (!tree)
+		return(1);
+	if (dup2(fd[0], STDIN_FILENO) == -1)
+		return (1);
+	if (dup2(fd[1], STDOUT_FILENO) == -1)
+		return (1);
+	close(fd[0]);
+	close(fd[1]);
+	return (0);
+}
+
 static int	define_stdin(t_tree *tree, int *fd, int permission)
 {
 	int	in_fd;
@@ -8,13 +21,15 @@ static int	define_stdin(t_tree *tree, int *fd, int permission)
 		return (1);
 	if (tree->signal == INPUT || tree->signal == HEREDOC)
 	{
-		in_fd = open(tree->left->node, permission);
+		in_fd = open(((char **)tree->left->node)[0], permission);
 		if (in_fd == -1)
 			return (1);
 		if (dup2(fd[0], STDIN_FILENO) == -1)
 			return (1);
 		close(in_fd);
 	}
+	else
+		base_redir(tree, fd);
 	return (0);
 }
 
@@ -26,13 +41,15 @@ static int	define_stdout(t_tree *tree, int *fd, int permission)
 		return (1);
 	if (tree->signal == OUTPUT || tree->signal == APPEND)
 	{
-		out_fd = open(tree->left->node, permission, 0644);
+		out_fd = open(((char **)tree->left->node)[0], permission, 0644);
 		if (out_fd == -1)
 			return (1);
 		if (dup2(fd[1], STDOUT_FILENO) == -1)
 			return (1);
 		close(out_fd);
 	}
+	else
+		base_redir(tree, fd);
 	return (0);
 }
 
@@ -51,29 +68,22 @@ static int	check_permission(t_tree *tree)
 	return (permission);
 }
 
-int	redirect(t_tree *tree, int *fd)
+int	redirect(t_tree **tree, int *fd)
 {
 	int	permission;
 
-	if (!tree)
+	if (!*tree)
 		return (1);
-	if (tree->signal < INPUT || tree->signal > HEREDOC)
+	if ((*tree)->signal >= INPUT && (*tree)->signal <= HEREDOC)
 	{
-		if (dup2(fd[0], STDIN_FILENO) == -1)
+		permission = check_permission(*tree);
+		if (define_stdin((*tree), fd, permission))
 			return (1);
-		if (dup2(fd[1], STDOUT_FILENO) == -1)
+		if (define_stdout((*tree), fd, permission))
 			return (1);
-		close(fd[0]);
-		close(fd[1]);
+		*tree = (*tree)->right;
 	}
 	else
-	{
-		permission = check_permission(tree);
-		if (define_stdin(tree, fd, permission))
-			return (1);
-		if (define_stdout(tree, fd, permission))
-			return (1);
-		tree = tree->right;
-	}
+		base_redir(*tree, fd);
 	return (0);
 }
