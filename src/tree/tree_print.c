137e88dfc6c7a973c02038433080c46b585909b9
buehara <buehara@student.42sp.org.br>
2026-04-19 18:44:12 -0300

    fix: corrections to datatype

--- src/tree/tree_print.c
+++ src/tree/tree_print.c
@@ -1,11 +1,11 @@
-#include "../includes/minishell.h"
+#include "../../includes/minishell.h"
 
 void	tree_print(t_tree *tree, int level)
 {
 	if (!tree)
 		return ;
 	tree_print(tree->right, level + 1);
-	printf("%*c%s\n", level * 5, ' ', (char *)tree->node);
+	printf("%*c%s\n", level * 5, ' ', ((char **)tree->node)[0]);
 	tree_print(tree->left, level + 1);
 }
 
@@ -14,6 +14,6 @@ void	tree_print_extense(t_tree *tree)
 	if (!tree)
 		return ;
 	tree_print_extense(tree->left);
-	ft_printf("%s ", (char *)tree->node);
+	ft_printf("%s ", ((char **)tree->node)[0]);
 	tree_print_extense(tree->right);
 }
