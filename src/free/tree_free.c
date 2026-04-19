9d700d07e8213db77fd7d27d04ffa4af25a26cf6
buehara <buehara@student.42sp.org.br>
2026-04-18 23:00:32 -0300

    chore: add new free char ** function

--- src/free/tree_free.c
+++ src/free/tree_free.c
@@ -1,11 +1,27 @@
 #include "../../includes/minishell.h"
 
+void	tree_node_free(char **node)
+{
+	int	index;
+
+	if (!node)
+		return;
+	index = 0;
+	while (node[index] != NULL)
+	{
+		free(node[index]);
+		index++;
+	}
+	node = NULL;
+}
+
 void	tree_free(t_tree *tree)
 {
 	if (!tree)
 		return ;
 	tree_free(tree->left);
-	free(tree->node);
+	if (tree->node)
+		tree_node_free((char **)tree->node);
 	tree_free(tree->right);
 	free(tree);
 }
