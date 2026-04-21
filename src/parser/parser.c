a69755567bd93d3b6fa5e587054a79a13e0e22f0
buehara <buehara@student.42sp.org.br>
2026-04-21 19:39:12 -0300

    chore: create file for parser

---
+++ src/parser/parser.c
@@ -0,0 +1,28 @@
+#include "../../includes/minishell.h"
+
+t_tree	*parser(char *input)
+{
+	char 			***tokens;
+	t_tokens_type	*signals;
+	t_check			flags;
+	t_token			*head;
+	t_tree			*tree;
+
+	tokens = lexer(input, &signals, &flags);
+	if (!tokens)
+	{
+		ft_printf("Lexer error: invalid syntax\n");
+		return (NULL);
+	}
+	head = token_create(tokens, signals);
+	if (!head)
+	{
+		envp_char_free(tokens);
+		free(signals);
+		return (NULL);
+	}
+	tree = tree_create(head, &flags);
+	envp_char_free(tokens);
+	free(signals);
+	return (tree);
+}
