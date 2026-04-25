bb448ad3cac45298c4573696c8d02d14523be46e
buehara <buehara@student.42sp.org.br>
2026-04-25 19:27:29 -0300

    fix: function size and flow

--- src/executor/execution_base.c
+++ src/executor/execution_base.c
@@ -17,10 +17,30 @@ static char	**create_path_table(t_envp *envp)
 	return (table);
 }
 
+static char	*find_fullpath(char *path, char **path_envp)
+{
+	int		index;
+	char	*full_path;
+
+	index = 0;
+	full_path = NULL;
+	while(path_envp[index] != NULL)
+	{
+		full_path = ft_strjoin(path_envp[index], path);
+		if (!full_path)
+			return (NULL);
+		if (access(full_path, F_OK | X_OK) == 0)
+			break;
+		free(full_path);
+		full_path = NULL;
+		index++;
+	}
+	return (full_path);
+}
+
 static char	*find_path(char **path_envp, char **cmd)
 {
 	char	*path;
-	int		index;
 	char	*full_path;
 
 	path = ft_strjoin("/", cmd[0]);
@@ -28,19 +48,9 @@ static char	*find_path(char **path_envp, char **cmd)
 		return (NULL);
 	if (access(path, X_OK | F_OK) == 0)
 		return (path);
-	index = 0;
-	while(path_envp[index] != NULL)
-	{
-		full_path = ft_strjoin(path_envp[index], path);
-		if (!full_path)
-			return (NULL);
-		if (access(full_path, F_OK | X_OK) == 0)
-			return (full_path);
-		free(full_path);
-		full_path = NULL;
-		index++;
-	}
-	return (NULL);
+	full_path = find_fullpath(path, path_envp);
+	free(path);
+	return (full_path);
 }
 
 
@@ -58,6 +68,7 @@ static int	base_exec(char **path_table, t_tree *tree, char **envp)
 	char	*path;
 	int		err;
 	char	**node;
+	pid_t	pid;
 
 	//if (builtin)
 		//Run builtin
@@ -65,37 +76,37 @@ static int	base_exec(char **path_table, t_tree *tree, char **envp)
 	err = 0;
 	path = NULL;
 	node = (char **)tree->node;
+
 	path = find_path(path_table, node);
 	if (!path)
 		return (1);
-	err = child_process(node, path, envp);
+	pid = fork();
+	if (pid == 0)
+	{
+		err = child_process(node, path, envp);
+	}
 	free(path);
+	waitpid(pid, &err, 0);
 	return (err);
 }
 
 int exec_pipe(t_tree *tree, char **path_table, int fd[2], char **envp)
 {
 	int		status_code;
-	pid_t	pid;
 
 	if (tree->signal == PIPE && pipe(fd) == -1)
 	{
 		envp_char_free(&path_table);
 		return (1);
 	}
-	pid = fork();
-	if (pid == 0)
-	{
-		if (tree->signal != CMD && redirect(&tree, fd) == 0)
-			return (1);
-		status_code = base_exec(path_table, tree, envp);
-	}
+	if (tree->signal != CMD && redirect(&tree, fd) == 0)
+		return (1);
+	status_code = base_exec(path_table, tree, envp);
 	if (tree->signal != CMD)
 	{
 		close(fd[0]);
 		close(fd[1]);
 	}
-	waitpid(pid, &status_code, 0);
 	return (status_code);
 }
 
@@ -116,13 +127,17 @@ int execution(t_tree *tree, t_envp *envp_table)
 		split_free(path_table);
 		return (1);
 	}
+	if (tree->signal >= INPUT && tree->signal <= HEREDOC)
+		if (redirect(&tree, fd))
+		{
+			split_free(path_table);
+			return (1);
+		}
 	if (tree->signal == CMD)
-	{
-        status_code = base_exec(path_table, tree, rebuilt_envp);
-    	return (status_code);
-	}
-	else if (tree->signal == PIPE)
+		status_code = base_exec(path_table, tree, rebuilt_envp);
+	else
 		status_code = exec_pipe(tree, path_table, fd, rebuilt_envp);
+	split_free(rebuilt_envp);
 	split_free(path_table);
 	return (status_code); // Temp to make the function compile
 	//TODO: Criar o export e adicionar o status_code dentro da variavel $?
