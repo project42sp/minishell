36ee47d95961142b80494813c7f19623329f897d
buehara <buehara@student.42sp.org.br>
2026-04-25 19:29:54 -0300

    fix: cleaning function with double free

--- src/free/tree_free.c
+++ src/free/tree_free.c
@@ -15,19 +15,16 @@ void	tree_node_free(char **node)
 	node = NULL;
 }
 
-void	tree_free(t_tree *tree)
+void	tree_free(t_tree **tree)
 {
-	if (!tree)
+	if (!tree || !*tree)
 		return ;
-	if (tree->left)
-		tree_free(tree->left);
-	if (tree->node)
-		tree_node_free((char **)tree->node);
-	tree_free(tree->right);
-	if (tree->node)
-	{
-		free(((char **)tree->node)[0]);// libera a string
-		free(tree->node);// libera o char **
-	}
-	free(tree);
+	if ((*tree)->left != NULL)
+		tree_free(&((*tree)->left));
+	if ((*tree)->node)
+		tree_node_free((char **)(*tree)->node);
+	if ((*tree)->right != NULL)
+		tree_free(&((*tree)->right));
+	free(*tree);
+	*tree = NULL;
 }
