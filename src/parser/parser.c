6fa91af3d7cc9b536ddb5a48e681df372bd9bec1
Thais Fernandes Rocha <thfernan@c1r3p2.42sp.org.br>
2026-05-02 19:41:43 -0300

    refactor: parser uses new lexer and drops intermediate conversions

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -2,28 +2,19 @@
 
 t_tree	*parser(char *input)
 {
-	char 			***tokens;
-	t_tokens_type	*signals;
 	t_check			flags;
-	t_token			*head;
+	t_token			*tokens;
 	t_tree			*tree;
 
-	tokens = lexer(input, &signals, &flags);
+	ft_bzero(&flags, sizeof(t_check));
+	tokens = lexer(input, &flags);
 	if (!tokens)
 	{
 		ft_printf("Lexer error: invalid syntax\n");
 		return (NULL);
 	}
-	head = token_create(tokens, signals);
-	if (!head)
-	{
-		envp_char_free(tokens);
-		free(signals);
-		return (NULL);
-	}
-	tree = tree_create(head, &flags);
-	token_list_free(head);
-	free(tokens);
-	free(signals);
+	debug_lexer(tokens);
+	tree = tree_create(tokens, &flags);
+	token_list_free(tokens);
 	return (tree);
 }
