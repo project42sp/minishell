8d018ce0e3e8bf25958f7a2cff49db05b1b5073b
Thais Fernandes Rocha <thfernan@c1r3p2.42sp.org.br>
2026-05-02 19:43:23 -0300

    chore: remove obsolete token creation file

--- src/token/token_cmd_create.c
+++ src/token/token_cmd_create.c
@@ -1,45 +1,28 @@
 #include "../../includes/minishell.h"
 
-static t_token	*token_node(char **token, t_tokens_type signal)
+t_token	*token_node(char *token_str, t_tokens_type signal)
 {
 	t_token	*node;
+	char	**arr;
 
-	node = (t_token *)ft_calloc(1, sizeof(t_token));
+	node = ft_calloc(1, sizeof(t_token));
 	if (!node)
 		return (NULL);
 	node->signal = signal;
-	node->token = token;
-	node->next = NULL;
-	return (node);
-}
-
-t_token	*token_create(char ***tokens, t_tokens_type *signal)
-{
-	t_token	*head;
-	t_token	*current;
-	t_token	*prev;
-	int		index;
-
-	if (!tokens || !signal)
-		return (NULL);
-	head = NULL;
-	prev = NULL;
-	index = 0;
-	while (signal[index] != EOFILE)
+	if (token_str)
 	{
-		current = token_node(tokens[index], signal[index]);
-		if (!current)
+		arr = ft_calloc(2, sizeof(char *));
+		if (!arr)
 		{
-			token_list_free(head);
+			free(node);
 			return (NULL);
 		}
-		if (!head)
-			head = current;
-		else
-			prev->next = current;
-		prev = current;
-		tokens[index] = NULL;
-		index++;
+		arr[0] = token_str;
+		arr[1] = NULL;
+		node->token = arr;
 	}
-	return (head);
+	else
+		node->token = NULL;
+	node->next = NULL;
+	return (node);
 }
