fe43f21794de7d9f1c91c72cc64778f6d488f8ea
Thais Fernandes Rocha <thfernan@c1r3p2.42sp.org.br>
2026-05-03 14:03:01 -0300

    refactor: add update_flags and token_free_partial helpers

--- src/lexer/lexer_utils.c
+++ src/lexer/lexer_utils.c
@@ -1,13 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lexer_utils.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thfernan <thfernan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/03 11:57:38 by thfernan          #+#    #+#             */
+/*   Updated: 2026/05/03 11:57:38 by thfernan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../includes/minishell.h"
 
-// Mover pra libft
 int	ft_isspace(char c)
 {
-	return (c == ' ' || c == '\t' || c == '\n' || c == '\v' || c == '\r' || c == '\f');
+	return (c == ' ' || c == '\t' || c == '\n' || c == '\v'
+		|| c == '\r' || c == '\f');
 }
 
 void	ft_skip_spaces(char *input, int *index)
 {
 	while (input[*index] && ft_isspace(input[*index]))
 		(*index)++;
+}
+
+void	update_flags(t_check *flags, t_tokens_type type)
+{
+	if (type == PIPE)
+		flags->pipe = 1;
+	else if (type == AND || type == OR)
+		flags->logical = 1;
+	else if (type == INPUT || type == HEREDOC)
+		flags->input = 1;
+	else if (type == OUTPUT || type == APPEND)
+		flags->output = 1;
+	else if (type == CMD || type == FILE_PATH)
+		flags->word = 1;
 }
