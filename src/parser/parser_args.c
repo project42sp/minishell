6b35240723bbe456b108a2753daa182c588e354a
Thais Fernandes Rocha <thfernan@c1r3p2.42sp.org.br>
2026-05-09 13:53:21 -0300

    refactor: aggregate multiple CMD tokens into single argv, enables commands with multiple arguments (ls -l)

---
+++ src/parser/parser_args.c
@@ -0,0 +1,46 @@
+#include "../../includes/minishell.h"
+
+static char	**add_arg(char **argv, char *new_str, int count)
+{
+	char	**new_argv;
+	int		i;
+
+	new_argv = ft_calloc(count + 2, sizeof(char *));
+	if (!new_argv)
+		return (NULL);
+	i = 0;
+	while (i < count)
+	{
+		new_argv[i] = argv[i];
+		i++;
+	}
+	new_argv[count] = new_str;
+	new_argv[count + 1] = NULL;
+	free(argv);
+	return (new_argv);
+}
+
+
+char	**collect_args(t_token **token)
+{
+	char	**argv;
+	int		count;
+	char	*str;
+
+	argv = NULL;
+	count = 0;
+	while (*token && (*token)->signal == CMD)
+	{
+		str = ((char **)(*token)->token)[0];
+		argv = add_arg(argv, str, count);
+		if (!argv)
+			return (NULL);
+		((char **)(*token)->token)[0] = NULL;
+		*token = (*token)->next;
+		count++;
+	}
+	if (count == 0)
+		argv = ft_calloc(1, sizeof(char *));
+	return (argv);
+}
+
