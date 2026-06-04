d180de30801b2f1e55ac173b10e284d9851b2864
Thais Fernandes Rocha <thfernan@c1r3p2.42sp.org.br>
2026-06-04 20:48:58 -0300

    fix: prevent invalid read in ft_exit by countig arguments

--- src/executor/builtin_exit.c
+++ src/executor/builtin_exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   builtin_exit.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: buehara <buehara@student.42sp.org.br>      +#+  +:+       +#+        */
+/*   By: thfernan <thfernan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/31 20:53:11 by buehara           #+#    #+#             */
-/*   Updated: 2026/05/31 20:53:13 by buehara          ###   ########.fr       */
+/*   Updated: 2026/06/04 18:06:47 by thfernan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,19 @@
 int	ft_exit(t_envp *envp, t_tree *tree, char **cmd)
 {
 	int	err;
+	int	argc;
 
-	err = 0;
 	if (!*cmd)
 		return (1);
-	if (cmd[2])
+	argc = 0;
+	while (cmd[argc])
+		argc++;
+	if (argc > 2)
 	{
 		ft_putendl_fd("minishell: exit: too many arguments", 2);
 		return (1);
 	}
+	err = 0;
 	if (cmd[1])
 		err = ft_atoi(cmd[1]);
 	envp_free(&envp);
